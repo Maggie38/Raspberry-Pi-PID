40b16cd2bc43e220e6bf9e501df5756f545f1f89
Maggie38 <69048844+Maggie38@users.noreply.github.com>
2024-02-16 04:50:36 -0500

    Update PIDController.cpp

--- PIDController.cpp
+++ PIDController.cpp
@@ -12,7 +12,7 @@ const int PWMPin = 5;
 
 // PID parameters to be altered accordingly
 float kp = 0.2;
-float ki = 0.0005 ;
+float ki = 0.0005;
 float kd = 0.5;
 
 float setpoint = 1;
@@ -33,7 +33,7 @@ void setup() {
 	// Use the motorPins array to set the pin modes
 	for (int i = 0; i < 2; i++) {
 		pinMode(motorPins[i][0], OUTPUT);
-	    pinMode(motorPins[i][1], OUTPUT);
+		pinMode(motorPins[i][1], OUTPUT);
 	}
 }
 
@@ -41,13 +41,13 @@ void setup() {
 void controlMotor(int motor, float output, bool b) {
 	//forward direction
 	if (b==true) {
-	    digitalWrite(motorPins[motor][0], HIGH);
-	    digitalWrite(motorPins[motor][1], LOW);
+		digitalWrite(motorPins[motor][0], HIGH);
+	    	digitalWrite(motorPins[motor][1], LOW);
 	}
 	//backward direction
 	else {
 	   	digitalWrite(motorPins[motor][0], LOW);
-	    digitalWrite(motorPins[motor][1], HIGH);
+		digitalWrite(motorPins[motor][1], HIGH);
 	}
 	//changing speed
 	analogWrite(PWMPin, (int)output);
