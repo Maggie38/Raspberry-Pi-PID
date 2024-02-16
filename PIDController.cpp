8e7c51da33b0f6d6824136b6de854d10f2a867c8
Maggie38 <69048844+Maggie38@users.noreply.github.com>
2024-02-16 16:18:57 -0500

    Add files via upload

--- PIDController.cpp
+++ PIDController.cpp
@@ -4,15 +4,12 @@
 const int signal1 = 2;
 const int signal2 = 3;
 
-// motor drivers (2 pins per motor)
-const int motorPins[2][2] = {{6, 7}, {8, 9}};
-
-// PWM pin for speed control
-const int PWMPin = 5;     
+// motor drivers (3 pins per motor: first 2 are for direction, third is for speed)
+const int motorPins[2][3] = {{6, 7, 3}, {8, 9, 5}};   
 
 // PID parameters to be altered accordingly
 float kp = 0.2;
-float ki = 0.0005;
+float ki = 0.0005 ;
 float kd = 0.5;
 
 float setpoint = 1;
@@ -25,15 +22,13 @@ float integral = 0;
 void setup() {
 	Serial.begin(9600);
 
-	pinMode(PWMPin, OUTPUT); 
-
 	pinMode(signal1, INPUT);
 	pinMode(signal2, INPUT);
 
 	// Use the motorPins array to set the pin modes
 	for (int i = 0; i < 2; i++) {
-		pinMode(motorPins[i][0], OUTPUT);
-		pinMode(motorPins[i][1], OUTPUT);
+		for (int j = 0; j < 3; j++)
+			pinMode(motorPins[i][j], OUTPUT);
 	}
 }
 
@@ -41,16 +36,16 @@ void setup() {
 void controlMotor(int motor, float output, bool b) {
 	//forward direction
 	if (b==true) {
-		digitalWrite(motorPins[motor][0], HIGH);
-	    	digitalWrite(motorPins[motor][1], LOW);
+	    digitalWrite(motorPins[motor][0], HIGH);
+	    digitalWrite(motorPins[motor][1], LOW);
 	}
 	//backward direction
 	else {
 	   	digitalWrite(motorPins[motor][0], LOW);
-		digitalWrite(motorPins[motor][1], HIGH);
+	    digitalWrite(motorPins[motor][1], HIGH);
 	}
 	//changing speed
-	analogWrite(PWMPin, (int)output);
+	analogWrite(motorPins[motor][2], (int)output);
 }
 
 void loop() {
@@ -58,8 +53,8 @@ void loop() {
 	input[0] = digitalRead(signal1);
 	input[1] = digitalRead(signal2);
 
-	// PID controller calculations
 	for (int i = 0; i < 2; i++) {
+		//PID controller calculations
     	float error = setpoint - input[i];
     	integral += (ki * error);
     	if (integral > 255) integral = 255;
